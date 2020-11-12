170dbb260abce79b296a671d5771d11719a814c8
PawelTomaszewski1GTP <62339772+PawelTomaszewski1GTP@users.noreply.github.com>
2020-11-12 11:24:11 +0100

    Add files via upload

---
+++ Untitled-2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
+int main(){
+
+
+cout << (0xaabb0099 & green);
+
+
+    int kolor2 = 0x0ff20012;
+
+    int czer, nieb, ziel, prze;
+
+
+
+    cout << "Jego red wynosi: " << (kolor2 & red) << endl;
+
+    cout << "Jego blue wynosi: " << (kolor2 & blue) << endl;
+
+    cout << "Jego green wynosi: " << (kolor2 & green) << endl;
+
+    cout << "Jego przezroczystosc wynosi: " << (kolor2 & 0x000000) << endl;
+
+
+int red = 0x00ff0000;
+
+    int green = 0x0000ff00;
+
+    int blue = 0x000000ff;
+}
+return 0;
