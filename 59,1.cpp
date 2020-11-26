4c57c6fddc2f7c884206a0e77d25434b6fb31038
PawelTomaszewski1GTP <62339772+PawelTomaszewski1GTP@users.noreply.github.com>
2020-11-26 11:17:07 +0100

    Add files via upload

---
+++ 59,1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+ 
+using namespace std;
+ 
+ int a;
+
+
+ int x= 0;
+ int y= 2;
+ 
+
+
+    while(a>0){
+        if(a%i==0)a=a/2;
+        else
+        return 0;
+        
+        for(int j=1;x>=j;j++)
+        {
+        	
+        	 if((i>2)&&(i%j==0))i++;
+        	 }
+        if(x==2)i++;}
+    
+    if(y==3) return 1;
+
+    else return 0;}
+    
+
+
+
+int main()
+{
+    fstream plik;
+
+    plik.open("jd.txt");
+ 
+
+    int a;
+
+
+    int x= 0;
+ 
+
+    while(!plik.eof())
+    { 
+    plik >> a;}
+
+    cout << "Wynik;"<<endl;
+ 
+
+
+
+
+
+
+    return 0;
+}
