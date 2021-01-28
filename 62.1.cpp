f68469fe039feafa1391c9f6a874f487db0423a2
PawelTomaszewski1GTP <62339772+PawelTomaszewski1GTP@users.noreply.github.com>
2021-01-28 10:59:44 +0100

    Update 62.1.cpp

--- 62.1.cpp
+++ 62.1.cpp
@@ -19,28 +19,29 @@ int ilosc = 0
     read.open("liczby1.txt");
     if (read.fail())
     {
-        cout<< "zły wynik\n";
+        cout<< "zły wynik";
         exit(1);
     }
 
     while (read>>liczba)
     {
+        
         if (liczba>max)
              max=liczba;
 
+        
         if (liczba<min)
              min=liczba;
 
                 sum=sum+liczba;
 
     }
-
-
-    cout<<"Największy : "<<max<<endl;
-    cout<<"Najmniejszy  "<<min<<endl;
+    cout<<"Największy:"<<max<<endl;
+    cout<<"Najmniejszy:"<<min<<endl;
 
 
     read.close();
+    
     write.close();
 
     return 0;
