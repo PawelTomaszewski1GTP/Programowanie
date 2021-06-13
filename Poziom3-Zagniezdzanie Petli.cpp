ef781289d43f5116aa5ed6cad32ab93e3734c71d
PawelTomaszewski1GTP <62339772+PawelTomaszewski1GTP@users.noreply.github.com>
2021-06-13 23:04:02 +0200

    Add files via upload

---
+++ Poziom3-Zagniezdzanie Petli.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() 
+{
+    
+    for (int x=1; x<4; x++) 
+    
+    {
+        for (int y=1; y<4; y++) 
+        
+        {
+            
+            for (int n=1; n<4; n++)
+            
+            {
+                
+                for (int i=1; i<4; i++)
+                
+                {
+                    
+                    for (int z=1; z<4; z++)
+                    
+                    {
+                        
+                        cout << x << " " << y << " " << n << " " << i << " " << z << endl;
+                        
+                    }
+                }
+            }
+        }
+    }
+ 
+    return 0;
+}
